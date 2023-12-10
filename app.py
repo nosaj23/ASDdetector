e85d193e06f0cb6cf9dc0e414c7271310440985c
JJ <87593941+nosaj23@users.noreply.github.com>
2023-12-10 18:53:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ st.image("images/ASD Header Option 2 (with kids).png")
 
 st.write("""    
     **Coach Nadia** utilizes Q-CHAT-10 (Quantitative Checklist for Autism in Toddlers) with proprietary machine learning algorithm to screen toddlers for signs of autism. 
-    **Typically applicable to children aged between 18 and 24 months (less than 4 years old)**, the Q-CHAT-10 serves as a tool for 
+    **Typically applicable to children aged between 18 and 48 months (less than 4 years old)**, the Q-CHAT-10 serves as a tool for 
     identifying children who may benefit a more comprehensive assessment for potential Autism Spectrum Disorders.
     """)
 
